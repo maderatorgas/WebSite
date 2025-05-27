846449ad6951c4f7aa7c0ad2872788a713fc5e87
bohdan <bog.tereschuck@gmail.com>
2025-05-27 19:19:14 +0300

    Add model & fix model

--- backend/backend/books/models.py
+++ backend/backend/books/models.py
@@ -1,13 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Genre(models.Model):
+    name = models.CharField()
+
+
 class Books(models.Model):
     title = models.CharField()
-    author = models.CharField()
-    genre = models.CharField()
+    genre = models.ForeignKey(Genre, on_delete=models.CASCADE)
     date_of_birth = models.DateField()
     description = models.TextField()
     price = models.FloatField()
     rating = models.FloatField()
     image = models.ImageField()
-    
