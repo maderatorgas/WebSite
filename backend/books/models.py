b2115ff27d3c8afff59ee2851c66cf72794de831
ChornaMamba <tymoshchuk.a2401.123.24@lntu.edu.ua>
2025-05-28 19:37:48 +0300

    Serializers
    Edited views.py
    Added serializers.py
    Added urla.py
    Edited models.py

--- backend/books/models.py
+++ backend/books/models.py
@@ -1,14 +1,16 @@
 from django.db import models
+import uuid
 
 
 class Genre(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField()
-
+    
 
 class Books(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(blank=False)
     genre = models.ForeignKey(Genre, on_delete=models.CASCADE, blank=False)
-    date_of_birth = models.DateField(blank=False)
     description = models.TextField(blank=False)
     price = models.FloatField(blank=False)
     rating = models.FloatField(blank=False)
--- backend/books/views.py
+++ backend/books/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
+from rest_framework import generics, mixins, permissions, filters
+from .models import Books
+from .serializers import BooksSerializer
 
-# Create your views here.
+class IsAdminOrReadOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:  # GET, HEAD, OPTIONS
+            return True
+        return request.user and request.user.is_staff
+
+class BookListCreateView(mixins.ListModelMixin,
+                         mixins.CreateModelMixin,
+                         generics.GenericAPIView):
+    queryset = Books.objects.all()
+    serializer_class = BooksSerializer
+    permission_classes = [IsAdminOrReadOnly]
+    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
+    search_fields = ['title', 'description']
+    ordering_fields = ['price', 'rating']
+
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+class BookDetailView(mixins.RetrieveModelMixin,
+                     mixins.UpdateModelMixin,
+                     mixins.DestroyModelMixin,
+                     generics.GenericAPIView):
+    queryset = Books.objects.all()
+    serializer_class = BooksSerializer
+    permission_classes = [IsAdminOrReadOnly]
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
