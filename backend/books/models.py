3faf11d29b8aa351c7fec108f5c8bb1aac3ccfd3
ChornaMamba <tymoshchuk.a2401.123.24@lntu.edu.ua>
2025-05-29 11:56:47 +0300

    Genre
    Edited models.py
    Added to serializers.py file Genre
    Added to views.py CRUD books genre
    Added to urls.py genre

--- backend/books/models.py
+++ backend/books/models.py
@@ -4,14 +4,14 @@ import uuid
 
 class Genre(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    name = models.CharField()
+    name = models.CharField(max_length=100)
     
 
 class Books(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    title = models.CharField(blank=False)
+    title = models.CharField(max_length=400, blank=False)
     genre = models.ForeignKey(Genre, on_delete=models.CASCADE, blank=False)
     description = models.TextField(blank=False)
     price = models.FloatField(blank=False)
     rating = models.FloatField(blank=False)
-    image = models.ImageField(blank=False)
+    image = models.ImageField(upload_to='book_images/', blank=False) #завантаження файлів в папку
--- backend/books/views.py
+++ backend/books/views.py
@@ -1,12 +1,16 @@
 from rest_framework import generics, mixins, permissions, filters
-from .models import Books
-from .serializers import BooksSerializer
+from .models import Books, Genre
+from .serializers import BooksSerializer, GenreSerializer
+
+#перевірка на адміна
 
 class IsAdminOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         if request.method in permissions.SAFE_METHODS:  # GET, HEAD, OPTIONS
             return True
         return request.user and request.user.is_staff
+
+#CRUD для книг
 
 class BookListCreateView(mixins.ListModelMixin,
                          mixins.CreateModelMixin,
@@ -40,3 +44,27 @@ class BookDetailView(mixins.RetrieveModelMixin,
 
     def delete(self, request, *args, **kwargs):
         return self.destroy(request, *args, **kwargs)
+
+#CRUD для жанрів
+
+class GenreListCreateView(mixins.ListModelMixin,
+                          mixins.CreateModelMixin,
+                          generics.GenericAPIView):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+    permission_classes = [IsAdminOrReadOnly]
+
+    def get(self, request, *args, **kwargs): return self.list(request, *args, **kwargs)
+    def post(self, request, *args, **kwargs): return self.create(request, *args, **kwargs)
+
+class GenreDetailView(mixins.RetrieveModelMixin,
+                      mixins.UpdateModelMixin,
+                      mixins.DestroyModelMixin,
+                      generics.GenericAPIView):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+    permission_classes = [IsAdminOrReadOnly]
+
+    def get(self, request, *args, **kwargs): return self.retrieve(request, *args, **kwargs)
+    def put(self, request, *args, **kwargs): return self.update(request, *args, **kwargs)
+    def delete(self, request, *args, **kwargs): return self.destroy(request, *args, **kwargs)
