6fcb90e432b884931319f7b640de09144a27f7a0
Білашук Павло <114417208+maderatorgas@users.noreply.github.com>
2025-05-29 21:53:17 +0300

    deleted

--- backend/books/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import BookListCreateView, BookDetailView, GenreListCreateView, GenreDetailView
-
-urlpatterns = [
-    path('', BookListCreateView.as_view(), name='book-list'),
-    path('<int:pk>/', BookDetailView.as_view(), name='book-detail'),
-    path('genres/', GenreListCreateView.as_view(), name='genre-list'),
-    path('genres/<int:pk>/', GenreDetailView.as_view(), name='genre-detail'),
-]
