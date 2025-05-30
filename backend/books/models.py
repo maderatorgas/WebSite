82c5c6ca9a6fc32b746d41329afe0fe3584644d5
bohdan <bog.tereschuck@gmail.com>
2025-05-30 12:57:21 +0300

    fix models

--- backend/books/models.py
+++ backend/books/models.py
@@ -3,7 +3,7 @@ import uuid
 
 
 class Genre(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=100)
     
 
